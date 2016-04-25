0863eef36316366b3c456259bc15778755f1dd0c
jm <jm@server>
2016-04-25 18:59:00 +0200

    mostly formatting, more details

--- www/js/loginx.js
+++ www/js/loginx.js
@@ -6,18 +6,45 @@
 /*global jQuery, window, document */
 define(['utils', 'theme', 'jquery'], function (utils, theme, jQuery) {
 
+    var name_map = {
+        "urn:oid:0.9.2342.19200300.100.1.3":"mail",
+        "urn:oid:1.2.840.113549.1.9.2":     "unstructuredName",
+        "urn:oid:1.3.6.1.4.1.5923.1.1.1.1": "eduPersonAffiliation",
+        "urn:oid:1.3.6.1.4.1.5923.1.1.1.3": "eduPersonOrgDN",
+        "urn:oid:1.3.6.1.4.1.5923.1.1.1.4": "eduPersonOrgUnitDN",
+        "urn:oid:1.3.6.1.4.1.5923.1.1.1.5": "eduPersonPrimaryAffiliation",
+        "urn:oid:1.3.6.1.4.1.5923.1.1.1.6": "eduPersonPrincipalName",
+        "urn:oid:1.3.6.1.4.1.5923.1.1.1.7": "eduPersonEntitlement",
+        "urn:oid:1.3.6.1.4.1.5923.1.1.1.8": "eduPersonPrimaryOrgUnitDN",
+        "urn:oid:1.3.6.1.4.1.5923.1.1.1.9": "eduPersonScopedAffiliation",
+        "urn:oid:1.3.6.1.4.1.5923.1.1.1.10":"eduPersonTargetedID (aka persistentID)	",
+        "urn:oid:1.3.6.1.4.1.25178.1.2.9":  "schacHomeOrganization",
+        "urn:oid:2.5.4.3": "cn",
+        "urn:oid:2.5.4.4": "surName",
+        "urn:oid:2.5.4.10": "organizationName",
+        "urn:oid:2.5.4.42": "givenName",
+    };
+
     function Loginx() {
+        this.met_refeds = "https://met.refeds.org/met/entity/{0}/?federation=edugain";
     }
 
     Loginx.prototype.list_loginx = function (obj_to_append) {
+        var self = this;
         utils.simple_ajax(
             settings.backend.api.list,
             function(data) {
                 var docs = data.result;
                 for (var i=0; i<docs.length; ++i) {
                     var doc = docs[i];
+                    var ts = doc.timestamp || doc.our_timestamp || "unknown";
+                    ts = ts.toString().replace("T", "").replace("Z", "");
+
+                    var idp = theme.link(doc.idp, self.met_refeds.format(doc.idp));
+                    var sp = theme.link(doc.sp, self.met_refeds.format(doc.sp));
+
                     obj_to_append.append(
-                        theme.list_login_item(doc.idp, doc.sp, doc.attributes, "...")
+                        theme.list_login_item(idp, sp, (doc.attributes || []).map(self.toname).sort(), ts, "...")
                     );
                 }
             },
@@ -25,6 +52,10 @@ define(['utils', 'theme', 'jquery'], function (utils, theme, jQuery) {
 
             }
         );
+    };
+
+    Loginx.prototype.toname = function(urn) {
+        return name_map[urn] || urn;
     };
 
 
--- www/js/theme.js
+++ www/js/theme.js
@@ -22,11 +22,11 @@ define(['utils', 'jquery'], function (utils, jQuery) {
         ];
     }
 
-    Html.prototype.list_login_item = function (idp, sp, attributes, result) {
+    Html.prototype.list_login_item = function (idp, sp, attributes, ts, result) {
         var attributes_html = "";
         if (attributes) {
             for (var i = 0; i < attributes.length; ++i) {
-                attributes_html += '<small class="text-muted">{0}</small> | '.format(attributes[i]);
+                attributes_html += '<h5 class="text-muted">{0}</h5>'.format(attributes[i]);
             }
         }else {
             attributes_html = "MISSING!";
@@ -34,14 +34,19 @@ define(['utils', 'jquery'], function (utils, jQuery) {
         var result_label = "label label-danger";
         return ('<div class="row entry">' +
                 '<div class="col-sm-10">' +
-                    '<h3>{0} -> {1}</h3>' +
+                    '<span class="text-muted pull-right"><i class="fa fa-clock-o" aria-hidden="true"></i> {5}</span>' +
+                    '<h3>{0} <i class="fa fa-sign-in text-muted" aria-hidden="true"></i> {1}</h3>' +
                     '<h4><span class="{2}">{3}</span></h4>' +
-                    '<h4>|{4}</h4>' +
+                    '{4}' +
                 '</div>' +
                 '<div class="col-sm-2">' +
                     '<a href="#" class="pull-right"><img src="./images/idp.png" class="img-circle"></a>' +
                 '</div>' +
-            '</div>').format(idp, sp, result_label, result, attributes_html);
+            '</div>').format(idp, sp, result_label, result, attributes_html, ts);
+    };
+
+    Html.prototype.link = function(text, href) {
+      return '<a href="{0}">{1}</a>'.format(href, text);
     };
 
     Html.prototype.loading_html = function(obj) {
